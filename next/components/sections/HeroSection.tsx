8abd0a39166d219aed6605556948c22191588c41
Cherifi, mohamed iyad <mc02589@surrey.ac.uk>
2026-02-08 16:32:01 +0000

    fixed arc croping on small screens

--- next/components/sections/HeroSection.tsx
+++ next/components/sections/HeroSection.tsx
@@ -11,7 +11,7 @@ const images = [
   "https://images.unsplash.com/photo-1519681393784-d120267933ba",
   "https://images.unsplash.com/photo-1520975916090-3105956dac38",
   "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
-    "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+  "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
   "https://images.unsplash.com/photo-1518770660439-4636190af475",
   "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
   "https://images.unsplash.com/photo-1519681393784-d120267933ba",
@@ -20,7 +20,7 @@ const images = [
 ];
 
   return (
-    <section id="home" className="w-full max-h-[1120px] overflow-x-hidden ">
+    <section id="home" className="w-full max-h-[1120px] overflow-hidden ">
       <div className="max-w-7xl mx-auto  relative">
         <ArcGalleryHero
         images={images}
